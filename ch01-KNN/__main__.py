27f86e686cee8a6270ed1d2147af98a9c862d807
myheed <sunzhuo@bupt.edu.cn>
2018-12-09 12:50:23 +0800

    feat:打印分类结果

--- ch01-KNN/__main__.py
+++ ch01-KNN/__main__.py
@@ -5,12 +5,8 @@ Created on Mon Jul  2 21:18:57 2018
 @author: Administrator
 """
 
-# from numpy import *
-# from Digit_recog import *
-from numpy import shape
 import matplotlib.pyplot as plt
-
-from Digit_recog import img2vec
+from Digit_recog import *
 from Feature_scaling import Norm_feature
 from KNN_algr import classify_KNN
 from Parse_data import file_parse_matrix
@@ -22,19 +18,21 @@ if __name__ == '__main__':
     print(classify_KNN([0, 0], group, labels, 3))
 
     DataMat, LabelMat = file_parse_matrix('datingTestSet2.txt')
-    print(DataMat, shape(DataMat), LabelMat)
+    # print(DataMat, shape(DataMat), LabelMat)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(DataMat[:, 1], DataMat[:, 2])
     plt.show()
+    print(1)
 
     dating_mat, label_mat = file_parse_matrix('datingTestSet2.txt')
+    # 归一化
     data_normed, ranges, minV = Norm_feature(dating_mat)
     Test_accuray(0.1, dating_mat, label_mat)
 
     testVec = img2vec('digits/testDigits/0_13.txt')
     print(testVec)
 
-    # HandWritingTest('digits/trainingDigits', 'digits/testDigits/')
+    HandWritingTest('digits/trainingDigits', 'digits/testDigits/')
     # 这行代码耗时比较久，可以单独运行
