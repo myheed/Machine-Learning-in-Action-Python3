4b972d82f2ae4e38a5a38452a1b3668797a6d12a
myheed <sunzhuo@bupt.edu.cn>
2018-12-09 14:34:29 +0800

    fix: spell error

--- ch01-KNN/Digit_recog.py
+++ ch01-KNN/Digit_recog.py
@@ -38,18 +38,18 @@ def HandWritingTest(train_dir, test_dir):
         train_mat[i, :] = img2vec('%s/%s' % (train_dir, fname))
     # labels is label_vec，同之前的KNN代码相同，存储标签
 
-    test_File_list = listdir(test_dir)
+    test_file_list = listdir(test_dir)
     error = 0.0
-    test_m = len(test_File_list)
+    test_m = len(test_file_list)
     for i in range(test_m):
-        fname = test_File_list[i]
+        fname = test_file_list[i]
         fstr = fname.split('.')[0]
         fnumber = int(fstr.split('_')[0])
         vec_test = img2vec('digits/testDigits/%s' % fname)
-        Result = classify_KNN(vec_test, train_mat, labels, 3)
+        result = classify_KNN(vec_test, train_mat, labels, 3)
         print("the classifier came with: %d, the real answer is :%d " \
-              % (Result, fnumber))
-        if (Result != fnumber): error += 1
+              % (result, fnumber))
+        if (result != fnumber): error += 1
     # 这部分和Test模块相同，直接copy过来就好
     print("the accuracy is %f | the error_rate is %f " % \
           (1 - (float(error) / float(test_m)), (float(error) / float(test_m))))
